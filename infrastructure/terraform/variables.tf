b17b0aa6a44ca03b0d6e002f260e480208aeb6d5
Brandon H <hurlburb@microsoft.com>
2020-03-02 11:31:15 -0800

    Adding default location

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -6,6 +6,7 @@ variable "prefix" {
 variable "location" {
   type        = string
   description = "Azure region where to create resources."
+  default     = "West US"
 }
 
 variable "eventGridFunctionName" {
